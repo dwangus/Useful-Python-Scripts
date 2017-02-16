9756c3dce30abc58cf8f922753e61d534e133376
David Wang <dwangus@gmail.com>
2017-02-16 17:55:14 -0500

    fixed import statement

--- graph.py
+++ graph.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
 import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import webbrowser as wwindow
-from __future__ import print_function
 
 ####################################################################################################
 commandHelp = '''
