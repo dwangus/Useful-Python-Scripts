3ddbb93d525697f298c6b544308f8fc629720a8d
David Wang <dwangus@gmail.com>
2017-02-16 19:05:39 -0500

    fixed several bugs

--- graph.py
+++ graph.py
@@ -55,6 +55,12 @@ Options for running script:
                                          lines for (NO spaces in 2nd argument!); if ommitted,
                                          plots data for each line (interpreting different
                                          lines depending on -c/-r)
+
+ - '-xclude __(int1),(int2),(...)__': which data-points, across all lines, you want to leave
+                                         out from data-set; i.e., if for x = 2,3,4, we have
+                                         Line1 = 8,9,14, and Line2 = 12,-2,3, setting the
+                                         flag "-xclude 0,1" would leave out Line1(2), Line2(2),
+                                         Line1(3), Line2(3). (ONLY INTEGERS FOR 2nd ARGUMENT)
                                          
  - '-xrange __(lower limit),(upper limit)__': lower and upper range for x-axis ticks
                                                 (NO spaces in 2nd argument!)
@@ -92,6 +98,7 @@ Options for running script:
                  either a photo viewer or matplotlib's utilities; shouldn't use this if
                  you're executing this script from command-line, unless you have some sort of
                  GUI for display
+
  - '-linename __(some base name for lines)__': basically to avoid typing in line names/labels
                                                  for many lines, if you set this flag with a
                                                  base line name, say "program", then for as many
@@ -127,8 +134,6 @@ def isFloat(s):
         return False
 
 def userInput(prompt, vers3):
-    return sys.stdin.readline().strip()
-    '''
     if vers3:
         return input(prompt)
     else:
@@ -137,7 +142,6 @@ def userInput(prompt, vers3):
         except NameError:
             print("Wrong input function based on version.")
             pass
-    '''
 ######################################################################
 
 
@@ -161,15 +165,17 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         if flags[opts["axis"]]:
             begin = 0
             if not any([isFloat(x) for x in data[0]]):
-                while not (True not in [isFloat(x) for x in data[begin]] and any([isFloat(x) for x in data[(begin+1)]])):
-                    begin += 1
-                    if begin > (len(data)-1):
+                increment = 0
+                while not (True not in [isFloat(x) for x in data[increment]] and any([isFloat(x) for x in data[(increment+1)]])):
+                    increment += 1
+                    if increment > (len(data)-1):
                         print("Something went wrong with parsing the data, with flags of -firstlabel and -c set.")
                         return
+                begin = increment + 1
             lineNames = [data[begin][n] for n in range(len(data[begin])) if n in selection and len(data[begin][n]) > 0]
             for i in selection:
                 lineData.append([])
-                for j in range((begin+1), len(data)):
+                for j in range(begin, len(data)):
                     lineData[-1].append(float(data[j][i]))
         else:
             if any([isFloat(x) for x in data[0]]):
@@ -190,14 +196,16 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         if flags[opts["axis"]]:
             begin = 0
             if not any([isFloat(x) for x in data[0]]):
-                while not (True not in [isFloat(x) for x in data[begin]] and any([isFloat(x) for x in data[(begin+1)]])):
-                    begin += 1
+                increment = 0
+                while not (True not in [isFloat(x) for x in data[increment]] and any([isFloat(x) for x in data[(increment+1)]])):
+                    increment += 1
                     if begin > (len(data)-1):
                         print("Something went wrong with parsing the data, with flags of -c set.")
                         return
+                begin = increment-1
             for i in selection:
                 lineData.append([])
-                for j in range((begin+1), len(data)):
+                for j in range(begin, len(data)):
                     lineData[-1].append(float(data[j][i]))
         else:
             if any([isFloat(x) for x in data[0]]):
@@ -220,6 +228,17 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
                 if l in selection:
                     lineData.append([float(d) for d in data[l][startingColumn:]])
 
+    if len(flags[opts["exclude"]]) > 0:
+        xp = flags[opts["exclude"]]
+        xpoints = [int(w.strip()) for w in xp.split(",") if isInt(w.strip())]
+        temp = []
+        for i in range(len(lineData)):
+            temp.append([])
+            for j in range(len(lineData[i])):
+                if j not in xpoints:
+                    temp[-1].append(lineData[i][j])
+        lineData = temp
+    
     if flags[opts["xaxis"]]:
         xs = lineData[0]
         lineData = lineData[1:]
@@ -227,8 +246,8 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         xs = [i for i in range(1, len(lineData[0])+1)]#Maybe this needs to be 0-indexed someday... but not today!
 
     defaults = {'mark': 'o', 'line':'--', 'loc':2,\
-                #'bbox':(1.05, 1)}
-                'bbox':None}
+                'bbox':(1.05, 1)}
+                #'bbox':None}
     markerShape = defaults['mark']
     lineStyle = defaults['line']
     locOption = defaults['loc']
@@ -266,11 +285,14 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
     for l in lineData:
+        #print(lineData.index(l), l)
         line, = ax.plot(xs, l, marker=markerShape, linestyle=lineStyle)
 
     if flags[opts["xrange"]][0] != flags[opts["xrange"]][1]:
+        print("\nX-axis Range: ", flags[opts["xrange"]])
         ax.set_xlim(flags[opts["xrange"]][0], flags[opts["xrange"]][1])
     if flags[opts["yrange"]][0] != flags[opts["yrange"]][1]:
+        print("Y-axis Range: ", flags[opts["yrange"]])
         ax.set_ylim(flags[opts["yrange"]][0], flags[opts["yrange"]][1])
 
     if not flags[opts["firstlabel"]]:
@@ -298,7 +320,7 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         else:
             for l in range(len(lineData)):
                 lineNames.append(userInput("Enter a name for line{}: ".format(l), is3))
-    print("Labels for Lines:\n{}".format(lineNames))
+    print("\nLabels for Lines:\n{}\n".format(lineNames))
     legend = ax.legend(lineNames, bbox_to_anchor=bboxAnchor, loc=locOption)
 
     if len(flags[opts["xlabel"]]) == 0:
@@ -306,12 +328,14 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         #ax.set_xlabel(sys.stdin.readline().strip())
         ax.set_xlabel(userInput("X-Axis Label: ", is3))
     else:
+        print("X-axis label of graph is: ", flags[opts["xlabel"]])
         ax.set_xlabel(flags[opts["xlabel"]])
     if len(flags[opts["ylabel"]]) == 0:
         #print("\nY-Axis Label: ", end='')
         #ax.set_ylabel(sys.stdin.readline().strip())
         ax.set_ylabel(userInput("Y-Axis Label: ", is3))
     else:
+        print("Y-axis label of graph is: ", flags[opts["ylabel"]])
         ax.set_ylabel(flags[opts["ylabel"]])
 
     if len(flags[opts["title"]]) == 0:
@@ -319,6 +343,7 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         #ax.set_title(sys.stdin.readline().strip())
         ax.set_title(userInput("Title of Graph: ", is3))
     else:
+        print("Title of graph is: ", flags[opts["title"]])
         ax.set_title(flags[opts["title"]])
 
     ax.grid('on')
@@ -333,6 +358,9 @@ def makeGraph(data, flags, opts, is3, appendToFilename=""):
         else:
             outputfilename = filenameParts[0] + appendToFilename + filenameParts[1]#base-name, numbering-order, extension
     fig.savefig(outputfilename, bbox_extra_artists=(legend,), bbox_inches='tight')
+
+    print("\n\nSaved file to name {}.".format(outputfilename))
+    print("####### Finished. #######\n\n")
     
     if is3 and flags[opts["display"]]:
         wwindow.open(outputfilename)
@@ -364,6 +392,8 @@ def main(fileData, flags, opts):
     
     if flags[opts["section"]] > -1:
         makeGraph(sections[flags[opts["section"]]], flags, opts, is3)
+    elif len(sections) == 1:
+        makeGraph(sections[0], flags, opts, is3)
     else:
         filenameCounter = 0
         for s in sections:
@@ -386,8 +416,9 @@ if __name__ == "__main__":
     usedArgs = [1] + [0]*(len(args)-1)
 
     opts = {"axis": 0, "firstlabel": 1, "section": 2, "select": 3, "xrange": 4, "yrange": 5, "xaxis": 6,\
-            "title":7, "xlabel":8, "ylabel":9, "beauty":10, "display":11, "savefile":12, "linename":13}
-    flags = [None, False, -1, [], (0,0), (0,0), False, "", "", "", False, False, "", ""]
+            "title":7, "xlabel":8, "ylabel":9, "beauty":10, "display":11, "savefile":12, "linename":13,\
+            "exclude":14}
+    flags = [None, False, -1, [], (0,0), (0,0), False, "", "", "", False, False, "", "", ""]
 
     ### Choose which axis of data to interpret as X/Y
     if '-c' in args:
@@ -451,7 +482,7 @@ if __name__ == "__main__":
         usedArgs[argIndex] = 1
         usedArgs[(argIndex+1)] = 1
         
-        temp = [float(x) for x in xrange.split(",")]
+        temp = [float(y) for y in yrange.split(",")]
         flags[opts["yrange"]] = (temp[0], temp[1])
 
     ### Determines if first row/column are the corresponding x-values for each line's data points
@@ -463,16 +494,19 @@ if __name__ == "__main__":
     if "-t" in args:
         flags[opts["title"]] = args[(args.index("-t") + 1)]
         usedArgs[args.index('-t')] = 1
+        usedArgs[(args.index('-t')+1)] = 1
 
     ### x-axis Label
     if "-x" in args:
         flags[opts["xlabel"]] = args[(args.index("-x") + 1)]
         usedArgs[args.index('-x')] = 1
+        usedArgs[(args.index('-x')+1)] = 1
 
     ### y-axis Label
     if "-y" in args:
         flags[opts["ylabel"]] = args[(args.index("-y") + 1)]
         usedArgs[args.index('-y')] = 1
+        usedArgs[(args.index('-y')+1)] = 1
 
     ### Flag set to determine if you want to manually enter in matplotlib graph parameters to "beautify" your graphs
     if "-beauty" in args:
@@ -489,21 +523,32 @@ if __name__ == "__main__":
         usedArgs[argIndex] = 1
         usedArgs[(argIndex+1)] = 1
 
+    if "-xclude" in args:
+        argIndex = args.index('-xclude')
+        flags[opts["exclude"]] = args[(argIndex + 1)]
+        usedArgs[argIndex] = 1
+        usedArgs[(argIndex+1)] = 1
+
     ### Output base filename (base meaning, if you're trying to make multiple graphs with
     ### one execution of this script, it'll just append incremented integers to the filename
     ### for each new graph it saves)
     if "-o" in args:
         flags[opts["savefile"]] = args[(args.index("-o") + 1)]
+        print("")
         print(args)
         usedArgs[args.index('-o')] = 1
     elif sum(usedArgs) == len(usedArgs)-1:
-        print(usedArgs)
+        print("")
         print(args)
+        #print(usedArgs)
         for i in range(len(usedArgs)):
             if usedArgs[i] == 0:
                 flags[opts["savefile"]] = args[i]
                 break
     else:
+        print("")
+        print(args)
+        print(usedArgs)
         print("\n***Warning***: should probably specify a base output filename for the saved picture...\n")
     
     try:
