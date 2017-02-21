c3bc8046c93ed827c7ebee1e124dbf85d838b019
David Wang <dwangus@gmail.com>
2017-02-21 16:33:12 -0500

    made delimiters more user-friendly

--- clip.py
+++ clip.py
@@ -8,12 +8,12 @@ comments = """
  - Must have arguments of times in 'start-end' (e.g. 30-40 (in terms of seconds)) format and
          specify output file with '-o some_outputfilename.mp4'.
 
- - Also, you can specify numbers as either 'X_Y' between X and Y seconds, or 'A-B_X-Y' for
+ - Also, you can specify numbers as either 'X-Y' between X and Y seconds, or 'A:B-X:Y' for
          clips between (A minutes, B seconds) to (X minutes, Y seconds) in the video.
  
  - Example usage of script:
  
-     \'python clip.py inputvideo_filename.mp4 3-5_4-16 12-1_12-45 -o some_output_filename.mp4\'
+     \'python clip.py inputvideo_filename.mp4 3:05-4:16 12:1-12:45 -o some_output_filename.mp4\'
      
    (This will basically clip inputvideo_filename.mp4 into 2 separate clips, one between 3:05 and
        4:16 minutes, another between 12:01 and 12:45 minutes -- and then stitch the two clips
@@ -41,13 +41,13 @@ def isFloat(s):
 def clip(args, inputfile, outputfile, separate=False):
     clips = []
     for time in args:
-        parsed = [t for t in time.split("_") if (isFloat(t) or ("-" in t and all([isFloat(m) for m in t.split("-")])))]
+        parsed = [t for t in time.split("-") if (isFloat(t) or (":" in t and all([isFloat(m) for m in t.split(":")])))]
         if len(parsed) != 2:
             print("Incorrect formatting of start-end argument for clipping video based on start and end times.")
             sys.exit()
         for p in range(len(parsed)):
-            if "-" in parsed[p]:
-                parsed[p] = parsed[p].split("-")
+            if ":" in parsed[p]:
+                parsed[p] = parsed[p].split(":")
                 parsed[p] = float(parsed[p][0])*60 + float(parsed[p][1])
             else:
                 parsed[p] = float(parsed[p])
